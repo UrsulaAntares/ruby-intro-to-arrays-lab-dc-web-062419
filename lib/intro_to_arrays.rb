c8eb05e90f123deb1ad4f4af2a06d22e408c6350
Ursula Miller <ursula.miller@gmail.com>
2019-06-10 03:32:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,6 +2,6 @@ def instantiate_new_array
   my_new_array = []
 end
 
-def aray_with_two_elements
-  my_two_array= ["Think zero" , "Thing 2"]
+def array_with_two_elements
+  my_two_array = ["Think zero" , "Thing 2"]
 end
