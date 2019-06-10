03644d1e479a4590a6208ad1481328dbc1491c0a
Ursula Miller <ursula.miller@gmail.com>
2019-06-10 03:39:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -21,11 +21,11 @@ def last_element(array)
 end
 
 
-def first_element_with_array_method(array)
+def first_element_with_array_methods(array)
   array.first
 end
 
-def last_element_with_array_method(array)
+def last_element_with_array_methods(array)
   array.last
 end
 
